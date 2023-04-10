1f8e907150f10ba897dec152ba2914cf620028f7
okdv <dev@otho.dev>
2023-04-10 10:23:12 -0500

    feat: added verbosity and configs

--- main.py
+++ main.py
@@ -1,56 +1,87 @@
 from urllib.request import Request, urlopen, ssl, socket
 from urllib.error import URLError, HTTPError
 from datetime import datetime, date
+from collections import OrderedDict
 import json, argparse, csv, re
 
 parser = argparse.ArgumentParser(description="bulk validate TLS certificates")
-parser.add_argument('-f', '--from', type=str, action="store", help="file path to csv of domains", default="./test-data.csv")
-parser.add_argument('-t', '--to', type=str, action="store", help="file path to csv result of TLS validations", default="./results.csv")
+parser.add_argument('-s', '--source', type=str, action="store", help="file path to csv of domains", default="data.csv")
+parser.add_argument('-d', '--dest', type=str, action="store", help="file path to csv result of TLS validations", default="results.csv")
 parser.add_argument('-c', '--column', action="store", help="header or index for column containing URLs to validate", default=0)
+parser.add_argument('-t', '--timeout', action="store", type=int, help="skip domains if response exceeds X seconds", default=30)
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', action="store_true", help="log everything")
+group.add_argument('-q', '--quiet', action="store_true", help="log nothing")
+group.add_argument('-T', '--threshold', action="store", type=int, help="log if expires within X days or less", default=7)
 
 args = parser.parse_args()
-varArgs = vars(args)
-from_file = varArgs['from']
-to_file = varArgs['to']
-col = varArgs['column']
 context = ssl.create_default_context()
 
+def printer(data, critical=False, threshold = 0):
+    if args.verbose:
+        print(data)
+    elif args.quiet:
+         pass
+    elif args.threshold >= threshold:
+        if critical:
+            print(data)
+        else:
+            pass
+
 def validate(hostname):
-    with socket.create_connection((hostname, '443'), timeout=30) as sock:
-        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            data = ssock.getpeercert()
-            valid_from = datetime.strptime(data['notBefore'], '%b %d %H:%M:%S %Y %Z').date()
-            valid_to = datetime.strptime(data['notAfter'], '%b %d %H:%M:%S %Y %Z').date()
-            today = date.today()
-            x_days_ago = (today - valid_from).days
-            x_days_to = (valid_to - today).days
+    with socket.create_connection((hostname, '443'), timeout=args.timeout) as sock:
+        try:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                data = ssock.getpeercert()
+                valid_from = datetime.strptime(data['notBefore'], '%b %d %H:%M:%S %Y %Z').date()
+                valid_to = datetime.strptime(data['notAfter'], '%b %d %H:%M:%S %Y %Z').date()
+                today = date.today()
+                x_days_ago = (today - valid_from).days
+                x_days_to = (valid_to - today).days
+                return {
+                    "domain": hostname,
+                    "valid": True if (x_days_ago >= 0 and x_days_to > 0) else False,
+                    "since": str(x_days_ago),
+                    "to": str(x_days_to)
+                }
+        except:
             return {
                 "domain": hostname,
-                "valid": True if (x_days_ago > 0 and x_days_to > 0) else False,
-                "since": str(x_days_ago),
-                "to": str(x_days_to)
+                "valid": False,
+                "since":"",
+                "to":""
             }
 
-with open(from_file, "r", newline='') as infile, open(to_file, "w", encoding="utf8", newline='') as outfile:
+with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding="utf8", newline='') as outfile:
     domains = set()
     writer = csv.writer(outfile)
     try:
-        col = int(col)
+        col = int(args.column)
         reader = csv.reader(infile)
+        printer("Treating --column as an index")
     except: 
-        col = str(col)
+        col = str(args.column)
         reader = csv.DictReader(infile)
+        printer("Treating --column as a header (string)")
 
     writer.writerow(['domain', 'passed TLS check', 'days since certificate start', 'days until certificate expire'])
     for row in reader:
-        hostname_search = re.search('^(?:https\:\/\/)?([a-zA-Z0-9-.:]*).*$', row[col], re.IGNORECASE)
-        if hostname_search and hostname_search.group(1):
-            domains.add(hostname_search.group(1))
+        if row[col] and len(row[col]) > 0:
+            hostname_search = re.search('^(?!http\:\/\/)(?:https\:\/\/)?([a-zA-Z0-9-.]*).*$', row[col], re.IGNORECASE)
+            if hostname_search and hostname_search.group(1):
+                domains.add(hostname_search.group(1))
+            else:
+                printer(row[col] + ": Skipped, please make sure this is a valid URL and it supports HTTPS", False)
+        else:
+            printer('Skipped empty row')
 
     for domain in domains:
+        printer("Validating " + domain + "...")
         try:
             obj = validate(domain)
-            print(obj)
-            writer.writerow([res['domain'], str(res['valid']), res['since'], res['to']])        
+            printer(obj, True, int(obj["to"]))
+            writer.writerow([obj["domain"], str(obj["valid"]), obj["since"], obj["to"]])        
         except:
+            printer(domain + ": Unable to validate, likely due to a timeout, unexpected response or invalid domain", True)
+            writer.writerow([domain, "False", "", "", "Possibly timed out, invalid response, or already expired certificate"])
             pass
