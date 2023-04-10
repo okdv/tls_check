6966456376e7381ad9e5b0cf41692db64ace5f1d
okdv <okd@otho.dev>
2023-04-10 14:05:27 -0500

    feat: preserve option

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from urllib.request import Request, urlopen, ssl, socket
 from urllib.error import URLError, HTTPError
 from datetime import datetime, date
 from collections import OrderedDict
-import json, argparse, csv, re, os
+import json, argparse, csv, re
 
 parser = argparse.ArgumentParser(description="bulk validate TLS certificates")
 parser.add_argument('-s', '--source', type=str, action="store", help="file path to csv of domains", default="data.csv")
 parser.add_argument('-d', '--dest', type=str, action="store", help="file path to csv result of TLS validations", default="results.csv")
 parser.add_argument('-c', '--column', action="store", help="header or index for column containing URLs to validate", default=0)
 parser.add_argument('-t', '--timeout', action="store", type=int, help="skip domains if response exceeds X seconds", default=30)
-parser.add_argument('-u', '--url-query', action="store", type=str, help="query URL for third party TLS validation service, use [DOMAIN] to inject domain into URL", default="https://www.ssllabs.com/ssltest/analyze.html?d=[DOMAIN]")
+parser.add_argument('-u', '--url-query', action="store", type=str, help="query URL for third party TLS validation service, use [DOMAIN] to inject domain into URL", default="https://www.ssllabs.com/ssltest/analyze.html?d=[DOMAIN]"),
+parser.add_argument('-p', '--preserve', action="store_true", help="preserve columns from --source, append after generated colomns in --dest")
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action="store_true", help="log everything")
@@ -40,25 +41,22 @@ def validate(hostname):
                 today = date.today()
                 x_days_ago = (today - valid_from).days
                 x_days_to = (valid_to - today).days
-                url_query = re.sub('\[DOMAIN\]', hostname, args.url_query)
                 return {
                     "domain": hostname,
                     "valid": True if (x_days_ago >= 0 and x_days_to > 0) else False,
                     "since": str(x_days_ago),
-                    "to": str(x_days_to),
-                    "url-query": url_query
+                    "to": str(x_days_to)
                 }
         except:
             return {
                 "domain": hostname,
                 "valid": False,
                 "since":"",
-                "to":"",
-                "url-query": url_query
+                "to":""
             }
 
 with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding="utf8", newline='') as outfile:
-    domains = set()
+    domains = list()
     writer = csv.writer(outfile)
     try:
         col = int(args.column)
@@ -69,26 +67,53 @@ with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding
         reader = csv.DictReader(infile)
         printer("Treating --column as a header (string)")
 
-    writer.writerow(['domain', 'passed TLS check', 'days since certificate start', 'days until certificate expire', 'external query url'])
+    header = ['domain', 'passed TLS check', 'days since certificate start', 'days until certificate expire', 'external query url']
+    if args.preserve:
+        try:
+            header.extend(reader.fieldnames)
+        except: 
+            printer("Could not locate headers to preserve")
+        printer("Preserving any original csv headers")
+
+    writer.writerow(header)
     for row in reader:
         if row[col] and len(row[col]) > 0:
             hostname_search = re.search('^(?!http\:\/\/)(?:https\:\/\/)?([a-zA-Z0-9-.]*).*$', row[col], re.IGNORECASE)
             if hostname_search and hostname_search.group(1):
-                domains.add(hostname_search.group(1))
+                domain_group = [hostname_search.group(1)]
+                if args.preserve:
+                    try:
+                        domain_group = domain_group + list(row.values())
+                    except:
+                        domain_group = domain_group + row
+
+                domains.append(domain_group)
             else:
                 printer(row[col] + ": Skipped, please make sure this is a valid URL and it supports HTTPS", False)
         else:
             printer('Skipped empty row')
 
-    for domain in domains:
+    for domain_list in domains:
+        domain = domain_list[0]
+        preserved_cols = domain_list[1:]
         printer("Validating " + domain + "...")
+        url_query = re.sub('\[DOMAIN\]', domain, args.url_query)
         try:
             obj = validate(domain)
             printer(obj, True, int(obj["to"]))
-            writer.writerow([obj["domain"], str(obj["valid"]), obj["since"], obj["to"], obj["url-query"]])        
+            row_list = [obj["domain"], str(obj["valid"]), obj["since"], obj["to"], url_query]
+            if args.preserve:
+                row_list.extend(preserved_cols)
+                printer("Preserving any remaining columns in row")
+
+            writer.writerow(row_list)        
         except:
             printer(domain + ": Unable to validate, likely due to a timeout, unexpected response or invalid domain", True)
-            writer.writerow([domain, "False", "", "", "Possibly timed out, invalid response, or already expired certificate"])
+            row_list = [domain, "False", "", "", "Possibly timed out, invalid response, or already expired certificate", url_query]
+            if args.preserve:
+                row_list.extend(preserved_cols)
+
+            writer.writerow(row_list)
             pass
 
     infile.close()
