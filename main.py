ad00d47ea68dcaa49b467a9e03e009e87b2fc5c5
okdv <okd@otho.dev>
2023-04-10 11:41:09 -0500

    fix: explicitly close source/dest

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from urllib.request import Request, urlopen, ssl, socket
 from urllib.error import URLError, HTTPError
 from datetime import datetime, date
 from collections import OrderedDict
-import json, argparse, csv, re
+import json, argparse, csv, re, os
 
 parser = argparse.ArgumentParser(description="bulk validate TLS certificates")
 parser.add_argument('-s', '--source', type=str, action="store", help="file path to csv of domains", default="data.csv")
 parser.add_argument('-d', '--dest', type=str, action="store", help="file path to csv result of TLS validations", default="results.csv")
 parser.add_argument('-c', '--column', action="store", help="header or index for column containing URLs to validate", default=0)
 parser.add_argument('-t', '--timeout', action="store", type=int, help="skip domains if response exceeds X seconds", default=30)
+parser.add_argument('-u', '--url-query', action="store", type=str, help="query URL for third party TLS validation service, use [DOMAIN] to inject domain into URL", default="https://www.ssllabs.com/ssltest/analyze.html?d=[DOMAIN]")
+
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action="store_true", help="log everything")
 group.add_argument('-q', '--quiet', action="store_true", help="log nothing")
@@ -38,18 +40,21 @@ def validate(hostname):
                 today = date.today()
                 x_days_ago = (today - valid_from).days
                 x_days_to = (valid_to - today).days
+                url_query = re.sub('\[DOMAIN\]', hostname, args.url_query)
                 return {
                     "domain": hostname,
                     "valid": True if (x_days_ago >= 0 and x_days_to > 0) else False,
                     "since": str(x_days_ago),
-                    "to": str(x_days_to)
+                    "to": str(x_days_to),
+                    "url-query": url_query
                 }
         except:
             return {
                 "domain": hostname,
                 "valid": False,
                 "since":"",
-                "to":""
+                "to":"",
+                "url-query": url_query
             }
 
 with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding="utf8", newline='') as outfile:
@@ -64,7 +69,7 @@ with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding
         reader = csv.DictReader(infile)
         printer("Treating --column as a header (string)")
 
-    writer.writerow(['domain', 'passed TLS check', 'days since certificate start', 'days until certificate expire'])
+    writer.writerow(['domain', 'passed TLS check', 'days since certificate start', 'days until certificate expire', 'external query url'])
     for row in reader:
         if row[col] and len(row[col]) > 0:
             hostname_search = re.search('^(?!http\:\/\/)(?:https\:\/\/)?([a-zA-Z0-9-.]*).*$', row[col], re.IGNORECASE)
@@ -80,7 +85,7 @@ with open(args.source, "r", newline='') as infile, open(args.dest, "w", encoding
         try:
             obj = validate(domain)
             printer(obj, True, int(obj["to"]))
-            writer.writerow([obj["domain"], str(obj["valid"]), obj["since"], obj["to"]])        
+            writer.writerow([obj["domain"], str(obj["valid"]), obj["since"], obj["to"], obj["url-query"]])        
         except:
             printer(domain + ": Unable to validate, likely due to a timeout, unexpected response or invalid domain", True)
             writer.writerow([domain, "False", "", "", "Possibly timed out, invalid response, or already expired certificate"])
